336c7af48309ea455b754bf0c978e463c6f27a1d
Park Jae Seong <timer973@naver.com>
2024-06-04 14:43:19 +0900

    불변 객체 - 값 변경

---
+++ src/lang/object/immutable/change/ImmutableMain2.java
@@ -0,0 +1,11 @@
+package lang.object.immutable.change;
+
+public class ImmutableMain2 {
+
+    public static void main(String[] args) {
+        ImmutableObj obj1 = new ImmutableObj(10);
+        obj1.add(20); // return한 값이 있지만
+
+        System.out.println("obj1 = " + obj1.getValue()); // obj1의 Value는 그대로다
+    }
+}
---
+++ src/lang/object/immutable/change/ImmutableObj.java
@@ -0,0 +1,21 @@
+package lang.object.immutable.change;
+
+public class ImmutableObj {
+
+    private final int value;
+
+    public ImmutableObj(int value) {
+        this.value = value;
+    }
+
+    public ImmutableObj add(int addValue) {
+        int result = value + addValue;
+        return new ImmutableObj(result); // 새로운 객체를 만들어서 반환. 기존의 인스턴스는 건들 수 없음.(final 불변)
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+
+}
