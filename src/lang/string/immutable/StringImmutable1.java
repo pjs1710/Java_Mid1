d650e95bb40a3ff76e40147850d52cb527405774
Park Jae Seong <timer973@naver.com>
2024-06-05 22:12:03 +0900

    String 클래스 - 불변 객체

---
+++ src/lang/string/immutable/StringImmutable1.java
@@ -0,0 +1,10 @@
+package lang.string.immutable;
+
+public class StringImmutable1 {
+
+    public static void main(String[] args) {
+        String str = "hello";
+        str.concat(" java"); // 반환값이 있음
+        System.out.println("str = " + str);
+    }
+}
