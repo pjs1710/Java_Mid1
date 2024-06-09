073e918b2df99c4ae84d2911275f8f48c7617493
Park Jae Seong <timer973@naver.com>
2024-06-09 15:44:56 +0900

    열거형 - 리팩토링1

---
+++ src/enumeration/ref1/ClassGrade.java
@@ -0,0 +1,19 @@
+package enumeration.ref1;
+
+public class ClassGrade {
+
+    public static final ClassGrade BASIC = new ClassGrade(10);
+    public static final ClassGrade GOLD = new ClassGrade(20);
+    public static final ClassGrade DIAMOND = new ClassGrade(30);
+
+    private final int discountPercent;
+
+    private ClassGrade(int discountPercent) {
+        this.discountPercent = discountPercent;
+    }
+
+    // 조회 메서드
+    public int getDiscountPercent() {
+        return discountPercent;
+    }
+}
