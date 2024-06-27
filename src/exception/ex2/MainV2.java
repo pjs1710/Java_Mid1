d2a96dea12eefbf47f229b1b1d176f60191d972c
Park Jae Seong <timer973@naver.com>
2024-06-27 13:43:29 +0900

    예외 처리 도입2 - 예외 복구

--- src/exception/ex2/MainV2.java
+++ src/exception/ex2/MainV2.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class MainV2 {
 
     public static void main(String[] args) throws NetworkClientExceptionV2 {
-        NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
+        // NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
+        NetworkServiceV2_2 networkService = new NetworkServiceV2_2();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
