3f4caa8bb49a291a49dffeaff274145821a5cc9d
Park Jae Seong <timer973@naver.com>
2024-06-27 19:33:14 +0900

    예외 계층2 - 활용

--- src/exception/ex3/MainV3.java
+++ src/exception/ex3/MainV3.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class MainV3 {
 
     public static void main(String[] args) {
-        NetworkServiceV3_1 networkService = new NetworkServiceV3_1();
+        // NetworkServiceV3_1 networkService = new NetworkServiceV3_1();
+        NetworkServiceV3_2 networkService = new NetworkServiceV3_2();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
