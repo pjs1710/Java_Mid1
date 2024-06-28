ea69c8073c9a7ddd8201db3fe6e48384d61cfc94
Park Jae Seong <timer973@naver.com>
2024-06-28 15:33:43 +0900

    try-with-resources

--- src/exception/ex4/MainV4.java
+++ src/exception/ex4/MainV4.java
@@ -7,7 +7,8 @@ import java.util.Scanner;
 public class MainV4 {
 
     public static void main(String[] args) {
-        NetworkServiceV4 networkService = new NetworkServiceV4();
+        // NetworkServiceV4 networkService = new NetworkServiceV4();
+        NetworkServiceV5 networkService = new NetworkServiceV5();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
